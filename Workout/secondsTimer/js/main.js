7c2f6a4c7e0de56f88b71d117fe5ad2d9d3129bc
saves7 <a_v.efimov@mail.ru>
2022-02-23 18:06:19 +0500

    seconds timer without stop

--- Workout/secondsTimer/js/main.js
+++ Workout/secondsTimer/js/main.js
@@ -1,4 +1,31 @@
-window.onload = function () {
+window.onload = function (e) {
+   
+    let buttons = document.querySelectorAll('.button-box__button');
+    let timmerInner = document.querySelector('.timer__content');
 
+   
 
-}
+    buttons[0].addEventListener('click', (e)=>{
+      
+        let setTimeoutStart = setTimeout(timer, 1000);
+
+        function timer () {
+            let date = new Date();
+            let seconds = date.getSeconds();
+            timmerInner.innerHTML = seconds;  
+            setTimeoutStart = setTimeout(timer, 1000);
+        }  
+        timer(); 
+
+        
+        buttons[1].addEventListener('click', () => {   
+            clearInterval(setTimeoutStart);       
+            timmerInner.innerHTML = "Счетчик остановлен";
+             
+        } );
+
+    }); 
+
+ 
+
+};
